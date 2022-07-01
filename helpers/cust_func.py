5c2bdfe8652b12fe26f2cd85fa1b42bfb827c541
Luke Moraglia <55449109+LukeMoraglia@users.noreply.github.com>
2022-07-01 11:46:51 -0400

    KNN ready

--- helpers/cust_func.py
+++ helpers/cust_func.py
@@ -2,9 +2,9 @@ from sklearn import metrics
 import pandas as pd
 
 def nice_conf_mat(y_true, y_pred, row_dict=None, column_dict=None):
-    if row_dict==None:
+    if row_dict is None:
         row_dict = {0:'True Negative', 1:'True Positive'}
-    if column_dict==None:
+    if column_dict is None:
         column_dict = {0:'Predicted Negative', 1:'Predicted Positive'}
     cm = metrics.confusion_matrix(y_true, y_pred)
     cm = pd.DataFrame(cm).rename(row_dict).rename(column_dict, axis=1)
